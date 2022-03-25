1b07c77292f1eca2bbaf328d479232f3589b0a4a
manichandu1998 <manichandu183@gmail.com>
2022-03-25 17:06:24 +0530

    Assignment 4 3,4,5 questions

--- StreamAvg.java
+++ StreamAvg.java
@@ -1,15 +1,27 @@
 package com.training.assignment;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamAvg {
 
 	public static void main(String[] args) {
-		List<Integer> arr =Arrays.asList(1,2,3,4,5,6,7,8);
-		Stream<Integer> str= arr.stream();
-		str.reduce((a,b)->a+b).ifPresent(System.out::println);
+		int[] arr = {1,2,3,4,5,6,7,8};
+		
+		
+		IntStream str= IntStream.of(arr);
+		OptionalDouble obj = str.average();
+		
+		if (obj.isPresent()) {
+            System.out.println(obj.getAsDouble());
+        }
+        else {
+            System.out.println("-1");
+        }
+
+		
 		
 
 	}
